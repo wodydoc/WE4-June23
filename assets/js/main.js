627bcfd1bd11c0865953d2a96ba6cdb83074b08e
Cody Dow <wodydoc@gmail.com>
2023-06-24 15:04:00 -0500

    hellohello

--- assets/js/main.js
+++ assets/js/main.js
@@ -149,18 +149,23 @@ const sr = ScrollReveal({
 
 sr.reveal(`.home__title, .home__data`);
 sr.reveal(`.home__social`, { delay: 600 });
-sr.reveal(`.about__title, .benefits__title, .benefits__data, .contact__box`, { origin: "left" });
+sr.reveal(`.about__title, .benefits__title, .benefits__data, .contact__box`, {
+  origin: "left",
+});
 sr.reveal(`.contact__form`, { origin: "right" });
 sr.reveal(`.processors, .stances, .types, .questions__group, .footer`, {
   interval: 100,
 });
 // Add the new animation for the SVG
-sr.reveal(".meet-joey-schewee__img, .meet-joey-schewee__title, .meet-joey-schewee__details-description", {
-  origin: "bottom",
-  distance: "20px",
-  duration: 1000,
-  delay: 400,
-});
+sr.reveal(
+  ".meet-joey-schewee__img, .meet-joey-schewee__title, .meet-joey-schewee__details-description",
+  {
+    origin: "bottom",
+    distance: "20px",
+    duration: 1000,
+    delay: 400,
+  }
+);
 
 sr.reveal(".slides-inner", {
   origin: "bottom",
@@ -267,17 +272,41 @@ const phrases = [
   "success",
 ];
 
-let currentIndex = 0;
 const phraseElement = document.querySelector(".random-word");
 
-const randomizeText = () => {
-  currentIndex = (currentIndex + 1) % phrases.length;
-  const newPhrase = phrases[currentIndex];
-  phraseElement.textContent = newPhrase;
+let currentIndex = 0;
+
+const animateText = () => {
+  // GSAP animation to fade out the current text
+  gsap.to(phraseElement, {
+    duration: 0.5,
+    opacity: 0,
+    onComplete: () => {
+      // Change the text when the animation is complete
+      currentIndex = (currentIndex + 1) % phrases.length;
+      phraseElement.textContent = phrases[currentIndex];
+
+      // GSAP animation to fade back in with the new text
+      gsap.to(phraseElement, {
+        duration: 0.5,
+        opacity: 1,
+      });
+    },
+  });
 };
 
-randomizeText();
-setInterval(randomizeText, 4000);
+// Initial call
+animateText();
+
+// Set an interval to repeat the animation every 4 seconds
+setInterval(animateText, 4000);
+
+
+
+
+// 
+
+// 
 
 // SPIN AND PARALLAX SCROLL
 function rotateAnimation(id, rotation) {
