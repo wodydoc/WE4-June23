1b01762875acae6da939b2d690ba2258f53cbc75
Cody Dow <wodydoc@gmail.com>
2023-07-08 18:28:39 -0500

    mi cocina

--- assets/js/stancescript.js
+++ assets/js/stancescript.js
@@ -5,11 +5,19 @@ window.addEventListener("DOMContentLoaded", function () {
 
   decks.forEach((deck) => {
     deck.addEventListener("mouseenter", () => {
-      gsap.to(deck, { rotationY: 180, duration: 0.5, ease: "power2.inOut" });
+      gsap.to(deck.querySelector(".deck-inner"), {
+        rotationY: 180,
+        duration: 1,
+        ease: "elastic.out(1, 0.3)",
+      });
     });
 
     deck.addEventListener("mouseleave", () => {
-      gsap.to(deck, { rotationY: 0, duration: 0.5, ease: "power2.inOut" });
+      gsap.to(deck.querySelector(".deck-inner"), {
+        rotationY: 0,
+        duration: 1,
+        ease: "elastic.out(1, 0.3)",
+      });
     });
   });
 });
