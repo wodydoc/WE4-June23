eea5076a12f5d4936ca50a8f64defc14b0928239
Cody Dow <wodydoc@gmail.com>
2023-07-11 22:23:56 -0500

    universal man

--- assets/js/universal-man.js
+++ assets/js/universal-man.js
@@ -1,10 +1,51 @@
-// Waving arms animation
-gsap.to("#a .b", {
-  rotation: 20,
-  transformOrigin: "50% 50%",
-  yoyo: true,
+gsap.registerPlugin(ScrollTrigger);
+
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".svg-container",
+    start: "top center",
+    end: "bottom top",
+    scrub: true,
+    onEnter: () => tl.play(),
+    onLeaveBack: () => tl.pause()
+  },
+});
+
+// Initial Animation - FadeIn
+gsap.from("#universal_man", {opacity: 0, duration: 1.5, ease: "power2.out"});
+
+// Rotate and Scale animation - we'll give it a playful twist by alternating the direction of rotation.
+tl.to("#universal_man", { 
+  rotation: 360, 
+  scale: 1.5, 
+  duration: 2, 
+  ease: "power1.inOut", 
   repeat: -1,
-  duration: 0.5,
+  yoyo: true
+})
+.to("#universal_man", { 
+  rotation: -360, 
+  duration: 2, 
+  ease: "power1.inOut", 
+  repeat: -1,
+  yoyo: true
+});
+
+// Pulsate animation to create a breathing effect
+tl.to("#universal_man", {
+  scale: 1.6,
+  repeat: -1,
+  yoyo: true,
+  duration: 1,
+  ease: "power2.inOut"
+});
+
+// Create an illusion of universal man drawing itself
+tl.from("#universal_man path", {
+  drawSVG: "0%", 
+  duration: 1,
+  stagger: 0.3,
+  ease: "power1.inOut",
   scrollTrigger: {
     trigger: ".svg-container",
     start: "top center",
