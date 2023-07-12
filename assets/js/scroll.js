88b9d222d61895043443b974a18df7fd5505911f
Cody Dow <wodydoc@gmail.com>
2023-07-12 13:32:02 -0500

    Update scrolljs

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,15 +1,10 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-function scrollHeader() {
-  const header = document.getElementById("header");
-  if (this.scrollY >= 80) {
-    header.classList.add("scroll-header");
-    header.style.backgroundColor = "var(--container-color)"; 
-  } else {
-    header.classList.remove("scroll-header");
-    header.style.backgroundColor = "var(--body-color)"; 
-  }
+function scrollHeader(){
+  const header = document.getElementById('header')
+  // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+  if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
-window.addEventListener("scroll", scrollHeader);
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll("section[id]");
