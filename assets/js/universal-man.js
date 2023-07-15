9e81ad91b018fb0079cf606fae417b302a4e15ea
Cody Dow <wodydoc@gmail.com>
2023-07-14 21:26:37 -0500

    lets go run

--- assets/js/universal-man.js
+++ assets/js/universal-man.js
@@ -1,54 +1,100 @@
-let lastScroll = 0;
-let rotationSpeed = 20; // Speed of rotation
+gsap.registerPlugin(ScrollTrigger);
 
-ScrollTrigger.create({
-  trigger: ".universal-man",
-  start: 'top bottom',
-  end: 'bottom top',
-  scrub: true,
-  onUpdate: (self) => {
-    let currentScroll = self.scroll;
-    let direction = (currentScroll > lastScroll) ? 1 : -1; // 1 means down, -1 means up.
+window.onload = function () {
+  const container = document.querySelector(".about__container");
+  const containerWidth = container.scrollWidth;
+  const panels = Array.from(container.querySelectorAll(".panel"));
+  let lastScroll = 0;
+  let rotationSpeed = 1;
 
-    rotationSpeed += direction * 0.5; // Increase or decrease speed based on direction
+  // Animation for the universal man
+  gsap.from(".universal-man", {
+    scale: 0,
+    rotation: 360,
+    duration: 1,
+    ease: "power1.out",
+  });
 
-    gsap.to(".universal-man", {
-      rotation: "+=" + (360 * direction), // This will add or subtract 360 to the rotation based on the scroll direction.
-      duration: rotationSpeed,
-      repeat: -1,
-      ease: "none"
+  // Animation for the title
+  gsap.from(".about__title", {
+    duration: 1,
+    y: -50,
+    ease: "power1.out",
+    scrollTrigger: {
+      trigger: ".universal-man",
+      start: "top 80%",
+      end: "bottom 20%",
+      toggleActions: "restart none none none",
+    },
+  });
+
+  // Horizontal scroll animation
+  gsap.to(container, {
+    x: -containerWidth + window.innerWidth,
+    ease: "none",
+    scrollTrigger: {
+      trigger: container,
+      pin: true,
+      scrub: true,
+      start: "top top",
+      end: () => `+=${containerWidth}`,
+      invalidateOnRefresh: true,
+    },
+  });
+
+  // Panel animations
+  panels.forEach((panel) => {
+    const deck = panel.querySelector(".deck");
+    const svg = deck.querySelector("svg");
+    const title = deck.querySelector("h1");
+
+    gsap.from([svg, title], {
+      opacity: 0,
+      scrollTrigger: {
+        trigger: panel,
+        start: "center center",
+        end: "center center",
+        scrub: true,
+        toggleActions: "play none none reverse",
+      },
     });
 
-    lastScroll = currentScroll;
-  }
-});
+    // 3D flip animation on hover
+    deck.addEventListener("mouseenter", () => {
+      gsap.to(deck.querySelector(".deck-inner"), {
+        rotationY: 180,
+        duration: 1,
+        ease: "elastic.out(1, 0.3)",
+      });
+    });
 
-gsap.from(".about__title", {
-  duration: 1,
-  autoAlpha: 0,
-  y: -50,
-  ease: 'power1.out',
-  scrollTrigger: {
-    trigger: ".about__title",
-    start: "top 80%",
-    end: "bottom 20%",
-    toggleActions: "restart none none none"
-  }
-});
+    deck.addEventListener("mouseleave", () => {
+      gsap.to(deck.querySelector(".deck-inner"), {
+        rotationY: 0,
+        duration: 1,
+        ease: "elastic.out(1, 0.3)",
+      });
+    });
+  });
 
-// Horizontal scroll for the .about__container
-let horizontalScroll = new gsap.timeline({
-  scrollTrigger: {
-    trigger: ".about__container",
-    pin: true,
-    scrub: 1,
-    start: 'top top',
-    end: () => "+=" + (document.querySelector(".about__container").offsetWidth - window.innerWidth)
-  }
-});
+  // Universal man rotation
+  ScrollTrigger.create({
+    trigger: ".universal-man",
+    start: "top bottom",
+    end: "bottom top",
+    scrub: true,
+    onUpdate: (self) => {
+      const currentScroll = self.scroll;
+      const direction = currentScroll > lastScroll ? 1 : -1;
+      rotationSpeed = Math.max(1, Math.min(40, rotationSpeed + direction));
 
+      gsap.to(".universal-man", {
+        rotation: "+=" + 360 * direction,
+        duration: rotationSpeed,
+        ease: "none",
+      });
 
-horizontalScroll.to(".playful-deck-container", {
-  x: -document.querySelector(".playful-deck-container").offsetWidth,
-  ease: 'none'
-});
+      lastScroll = currentScroll;
+    },
+  });
+};
